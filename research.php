5e2078325d9f132748499be3fe2d3cb2571c8894
deansunny <huangdi.hdu@gmail.com>
2016-04-21 15:00:35 +0800

    research.

---
+++ research.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>研究内容</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<meta name="keywords" content="Play-Offs Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<script type="application/x-javascript"> 
+			addEventListener("load", function() {
+				setTimeout(hideURLbar, 0); }, false); 
+				function hideURLbar(){ window.scrollTo(0,1); } 
+        </script>
+		<!meta charset utf="8">
+		<!--owlcss-->
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<!--bootstrap-->
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<!--coustom css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<!--default-js-->
+		<script src="js/jquery-2.1.4.min.js"></script>
+		<!--bootstrap-js-->
+		<script src="js/bootstrap.min.js"></script>
+		<!--script-->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<!--script-->
+</head>
+
+<body>
+<div id="container" class="container">
+  <div class="header_nav">
+      <nav class="navbar navbar-default chn-gd">
+          <div style="height:126px;">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <div class="logo-head-img">
+                  <img id="logo-head" src="images/logo_web.png"/>
+              </div>
+              <div class="logo-head-txt">
+                  <!--<a class="navbar-brand logo-st" href="index.php">CAMALAB</a>-->
+                  CAMALAB
+              </div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <!-- /.navbar-collapse -->
+          </div>
+          <div class="navigation">
+          <ul class="nav navbar-nav navbar-right">
+              <li>
+              <a href="./index.php" class="scroll">
+              首页
+              </a>
+              </li>
+              <!---->
+              <li class="tab-focus">
+              <a href="./research.php" class="scroll">
+
+              研究内容
+              </a>
+              </li>
+              <!---->
+              <li>
+              <a href="./paper.php" class="scroll">
+              论文
+              </a>
+              </li>
+              <!---->
+              <li>
+              <a href="./member.php" class="scroll">
+              
+              成员
+              </a>
+              </li>
+              <!---->
+              <li>	
+              <a href="./aboutus.php" class="scroll">
+              
+              关于我们
+              </a>
+              </li>
+              <!---->
+              <li>	
+              <a href="./link.php" class="scroll">
+              
+              相关链接
+              </a>
+              </li>
+             
+          </ul>
+          </div>
+          </nav>
+      </div>
+      
+      	 
+  </div>
+  <div class="main">
+  
+  </div>
+  <div class="footer">
+      <div class="footer-text">
+          <!--<h3 style="font-family:'微软雅黑', sans-serif, serif"><a href="index.html">杭州电子科技大学</a></h3>-->
+          <!--<p>Copyright &copy; 2016.CAMALAB</p>-->
+          <a href="http://www.hdu.edu.cn"><img src="images/logo.png"  /></a>
+      </div>
+  </div>
+  <a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+</div>
+
+</body>
+</html>
