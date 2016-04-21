89abaeeb12cdfac750ee190b96795f2f29e825f8
deansunny <huangdi.hdu@gmail.com>
2016-04-21 15:00:05 +0800

    index.

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome to CAMALAB!</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+		<meta name="keywords" content="Play-Offs Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<script type="application/x-javascript"> 
+			addEventListener("load", function() {
+				setTimeout(hideURLbar, 0); }, false); 
+				function hideURLbar(){ window.scrollTo(0,1); } 
+        </script>
+		<!meta charset utf="8">
+		<!--owlcss-->
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<!--bootstrap-->
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<!--coustom css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<!--default-js-->
+		<script src="js/jquery-2.1.4.min.js"></script>
+		<!--bootstrap-js-->
+		<script src="js/bootstrap.min.js"></script>
+		<!--script-->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<!--script-->
+</head>
+
+<body>
+<div id="container" class="container">
+  <div class="header_nav">
+      <nav class="navbar navbar-default chn-gd">
+          <div class="logo-title">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <div class="logo-head-img">
+                  <img id="logo-head" src="images/logo_web.png"/>
+              </div>
+              <div class="logo-head-txt">
+                  <!--<a class="navbar-brand logo-st" href="index.php">CAMALAB</a>-->
+                  CAMALAB
+              </div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <!-- /.navbar-collapse -->
+          </div>
+          <div class="navigation">
+          <ul class="nav navbar-nav navbar-right">
+              <li class="tab-focus">
+              <a href="./index.php" class="scroll">
+              首页
+              </a>
+              </li>
+              <!---->
+              <li>
+              <a href="./research.php" class="scroll">
+
+              研究内容
+              </a>
+              </li>
+              <!---->
+              <li>
+              <a href="./paper.php" class="scroll">
+              论文
+              </a>
+              </li>
+              <!---->
+              <li>
+              <a href="./member.php" class="scroll">
+              
+              成员
+              </a>
+              </li>
+              <!---->
+              <li>	
+              <a href="./aboutus.php" class="scroll">
+              
+              关于我们
+              </a>
+              </li>
+              <!---->
+              <li>	
+              <a href="./link.php" class="scroll">
+              
+              相关链接
+              </a>
+              </li>
+              
+          </ul>
+          </div>
+          </nav>
+      </div>
+      
+      <div class="header_banner">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+              <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <!--<li data-target="#myCarousel" data-slide-to="3"></li>-->
+              </ol>
+  
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+              <div class="item active  image-wid">
+                  <img src="./images/resize_1.jpg" alt="..." class="img-responsive">
+                  
+                  <div class="carousel-caption">
+                      <!--<h3> </h3>
+                      <p>&nbsp;&nbsp;&nbsp;&nbsp;Xiamen Hu Chuang Technology Co.</p>
+                      <button type="button" class="btn btn-info sld" onClick="window.location.href = 'introduce.php'" >了解更多</button>-->
+                  </div>
+              </div>
+              <div class="item  image-wid">
+                  <img src="./images/resize_2.jpg" alt="..." class="img-responsive">
+                  <div class="carousel-caption">
+                      <!--<h3></h3>
+                      <p>&nbsp;&nbsp;&nbsp;&nbsp;LOGO Recognition Detection</p>
+                      <button type="button" class="btn btn-info sld" onClick="window.location.href = 'logo_introduce.php'">了解更多</button>-->
+                  </div>
+              </div>
+              <div class="item  image-wid">
+                  <img src="./images/resize_3.jpg" alt="..." class="img-responsive">
+                  <div class="carousel-caption">
+                      <!--<h3></h3>
+                      <p>&nbsp;&nbsp;&nbsp;&nbsp;Human Action Recognition</p>
+                      <button type="button" class="btn btn-info sld" onClick="window.location.href = 'action_introduce.php'">了解更多</button>-->
+                  </div>
+              </div>
+              <!--<div class="item  image-wid">
+              <img src="./images/1l.jpg" alt="..." class="img-responsive">
+              <div class="carousel-caption">
+                  <h3>Viral Treatments</h3>
+                  <p>Lorem Ip sum is simply dummy text of the typesetting industry</p>
+                  <button type="button" class="btn btn-info sld">了解更多</button>
+              </div>
+              </div>-->
+          </div>
+          <!-- Controls -->
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+          </a>
+      </div>	 
+  </div>
+  <div class="main">
+  
+  </div>
+  <div class="footer">
+      <div class="footer-text">
+          <!--<h3 style="font-family:'微软雅黑', sans-serif, serif"><a href="index.html">杭州电子科技大学</a></h3>-->
+          <!--<p>Copyright &copy; 2016.CAMALAB</p>-->
+          <a href="http://www.hdu.edu.cn"><img src="images/logo.png"  /></a>
+      </div>
+  </div>
+  <a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+</div>
+
+</body>
+</html>
