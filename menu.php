6ff9d5c8d7b4dd3fdee7a48161d617897a01dcf3
deansunny <huangdi.hdu@gmail.com>
2016-04-29 16:12:59 +0800

    menu flie

---
+++ menu.php
@@ -0,0 +1,61 @@
+<div id="container" class="container">
+  <div class="header_nav">
+      <nav class="navbar navbar-default chn-gd">
+          <div class="logo-title">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <div class="logo-head-img">
+                  <img id="logo-head" src="./images/logo_web.png"/>
+              </div>
+              <div class="logo-head-txt">
+                  <!--<a class="navbar-brand logo-st" href="index.php">CAMALAB</a>-->
+                  CAMALAB
+              </div>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <!-- /.navbar-collapse -->
+          </div>
+          <div class="navigation">
+          <ul class="nav navbar-nav navbar-right">
+              <li id="ind">
+                  <a href="./index.php" class="scroll">
+                  	首页
+                  </a>
+              </li>
+              <!---->
+              <li id="res">
+                  <a href="./research.php" class="scroll">
+                  	研究内容
+                  </a>
+              </li>
+              <!---->
+              <li id="pap">
+                  <a href="./paper.php" class="scroll">
+                  	论文
+                  </a>
+              </li>
+              <!---->
+              <li id="mem">
+                  <a href="./member.php" class="scroll">
+                  	成员
+                  </a>
+              </li>
+              <!---->
+              <li id="abo">	
+                  <a href="./aboutus.php" class="scroll">
+                  	关于我们
+                  </a>
+              </li>
+              <!---->
+              <li id="lin">	
+                  <a href="./link.php" class="scroll">                 
+                  	相关链接
+                  </a>
+              </li>
+          </ul>
+          </div>
+          </nav>
+      </div>
+      
+      	 
+  </div>
